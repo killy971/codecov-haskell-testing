c35f0f8c2905c977f9d8ba4993b97ae5972e20db
Guillaume Nargeot <killy971+github@gmail.com>
2014-09-18 23:33:26 +0900

    Use curl command verbose mode

--- send-fake-coverage.sh
+++ send-fake-coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -H 'Content-Type: application/json' -d '{ "coverage": { "src/Util.hs": [null, null, null, 0, true, 1] } }' https://codecov.io/upload/v1?travis_job_id=$TRAVIS_BUILD_NUMBER&commit=$TRAVIS_COMMIT&branch=develop&travis_job_id=$TRAVIS_JOB_ID
+curl -v -X POST -H 'Content-Type: application/json' -d '{ "coverage": { "src/Util.hs": [null, null, null, 0, true, 1] } }' https://codecov.io/upload/v1?travis_job_id=$TRAVIS_BUILD_NUMBER&commit=$TRAVIS_COMMIT&branch=develop&travis_job_id=$TRAVIS_JOB_ID
