bd3d184c3133a599cdf303b8f2fc29f81e8322d9
Guillaume Nargeot <killy971+github@gmail.com>
2014-09-18 23:28:15 +0900

    Use travis_job_id instead of token in the curl command

--- send-fake-coverage.sh
+++ send-fake-coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -H 'Content-Type: application/json' -d '{ "coverage": { "src/Util.hs": [null, null, null, 0, true, 1] } }' https://codecov.io/upload/v1?token=dbd3e4a7-86e2-4537-b607-142bf46d7766&commit=$TRAVIS_COMMIT&branch=develop&travis_job_id=$TRAVIS_JOB_ID
+curl -X POST -H 'Content-Type: application/json' -d '{ "coverage": { "src/Util.hs": [null, null, null, 0, true, 1] } }' https://codecov.io/upload/v1?travis_job_id=$TRAVIS_BUILD_NUMBER&commit=$TRAVIS_COMMIT&branch=develop&travis_job_id=$TRAVIS_JOB_ID
