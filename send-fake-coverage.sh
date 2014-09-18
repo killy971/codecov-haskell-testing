254cfe8fc4d9aa43e773cf62effb79a2db1980e1
Guillaume Nargeot <killy971+github@gmail.com>
2014-09-18 23:42:31 +0900

    Display curl command return code

--- send-fake-coverage.sh
+++ send-fake-coverage.sh
@@ -3,3 +3,5 @@
 echo "Running curl command with travis_job_id=$TRAVIS_BUILD_ID and commit=$TRAVIS_COMMIT"
 
 curl -v -X POST -H 'Content-Type: application/json' -d '{ "coverage": { "src/Util.hs": [null, null, null, 0, true, 1] } }' https://codecov.io/upload/v1?travis_job_id=$TRAVIS_BUILD_NUMBER&commit=$TRAVIS_COMMIT&branch=develop
+
+echo "Return code: $?"
