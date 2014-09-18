2d12adca055c0fba35dd1ee4f65688a2bd1c19ea
Guillaume Nargeot <killy971+github@gmail.com>
2014-09-19 00:06:11 +0900

    use  instead of

--- send-fake-coverage.sh
+++ send-fake-coverage.sh
@@ -2,6 +2,6 @@
 
 echo "Running curl command with travis_job_id=$TRAVIS_BUILD_ID and commit=$TRAVIS_COMMIT"
 
-curl -v -X POST -H 'Content-Type: application/json' -d '{ "coverage": { "src/Util.hs": [null, null, null, 0, true, 1] } }' https://codecov.io/upload/v1?travis_job_id=$TRAVIS_BUILD_NUMBER&commit=$TRAVIS_COMMIT&branch=develop
+curl -v -X POST -H 'Content-Type: application/json' -d '{ "coverage": { "src/Util.hs": [null, null, null, 0, true, 1] } }' https://codecov.io/upload/v1?travis_job_id=$TRAVIS_JOB_ID&commit=$TRAVIS_COMMIT&branch=develop
 
 echo "Return code: $?"
